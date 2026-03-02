2bdfff9d85219ab0dacda116aa4a3e25e173e7aa
Cristian Antonio Escalante Hernandez <eh18010@ues.edu.sv>
2026-03-02 15:37:30 -0600

    feat: add 6.video.py file

---
+++ 6.video.py
@@ -0,0 +1,29 @@
+print("Tipo de datos en Python")
+x = 10
+y = 5.678
+print(type(x))
+print(type(y))
+
+print("En Python existen varios tipos de datos, entre ellos se encuentran los números enteros (int) y los números decimales (float)")
+print("Ejemplo: x = 10 (int), y = 5.678 (float))")
+
+z = 1.2e6
+print(type(z))
+print(z)
+print("En Python también se pueden representar números en notación científica utilizando la letra e para indicar la potencia de 10")
+
+print("Para comentar texto en python se utiliza el símbolo #, todo lo que esté después de este símbolo en la misma línea será considerado un comentario y no será ejecutado por el intérprete de Python")
+
+print("*"*30)
+is_true = True
+is_false = False
+print(type(is_true))
+print(type(is_false))
+print("En Python también existen los tipos de datos booleanos (bool), que pueden tener dos valores: True (verdadero) y False (falso)")
+
+
+
+
+
+
+
