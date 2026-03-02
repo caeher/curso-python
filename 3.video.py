12f3368257043e5a62d06f60d9568e1b10062045
Cristian Antonio Escalante Hernandez <eh18010@ues.edu.sv>
2026-03-02 15:20:05 -0600

    feat: add 3.video.py file

---
+++ 3.video.py
@@ -0,0 +1,12 @@
+print("Hola")
+print("Mundo")
+
+print("La identación es importante en Python")
+
+saludo = "Hola mundo"
+print("Hola mundo desde una variable")
+print(saludo)
+
+saludo = "Hola"
+nombre = "Cristian"
+print(saludo + " " + nombre)
