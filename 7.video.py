ce55e069986182231407aa2c93d317d244cf6a23
Cristian Antonio Escalante Hernandez <eh18010@ues.edu.sv>
2026-03-02 15:42:27 -0600

    feat: add 7.video.py file

---
+++ 7.video.py
@@ -0,0 +1,13 @@
+print("Nunca", "pares", "de", "aprender")
+
+print("Uso de separadores en la función print()")
+print("Hola", "Mundo", sep="-")
+print("Hola", "Mundo", sep="***")
+
+print("Uso de terminadores en la función print()")
+print("Hola", end=" ")
+print("Mundo")
+print("Hola", end="***")
+print("Mundo")
+
+print("En la función print() se pueden utilizar los parámetros sep y end para personalizar el separador entre los argumentos y el terminador al final de la impresión, respectivamente")
