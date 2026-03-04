8490b7e4b5a6363074eb50e6ab529a75984e14f8
Cristian Antonio Escalante Hernandez <eh18010@ues.edu.sv>
2026-03-03 22:06:19 -0600

    feat: add 8.video.py file

---
+++ 8.video.py
@@ -0,0 +1,47 @@
+# Operadores numericos
+a = 10
+b = 3
+print("Suma: ", a + b)
+print("Resta: ", a - b)
+print("Multiplicación: ", a * b)
+print("División: ", a / b)
+print("División entera: ", a // b)
+print("Potencia: ", a ** b)
+print("Módulo: ", a % b)
+
+print("a = 10\nb = 3")
+print("Suma se usa +, ejemplo: a + b ", a + b, sep="= ")
+print("Resta se usa -, ejemplo: a - b ", a - b, sep="= ")
+print("Multiplicación se usa *, ejemplo: a * b ", a * b, sep="= ")
+print("División se usa /, ejemplo: a / b ", a / b, sep="= ")
+print("División entera se usa //, ejemplo: a // b ", a // b, sep="= ")
+print("Potencia se usa **, ejemplo: a ** b ", a ** b, sep="= ")
+print("Módulo se usa %, ejemplo: a % b ", a % b, sep="= ")
+
+print("Forma de autoasignación de operadores")
+# a += b
+print("a += b es lo mismo que a = a + b", a, sep="= ")
+# a -= b
+print("a -= b es lo mismo que a = a - b", a, sep="= ")
+# a *= b
+print("a *= b es lo mismo que a = a * b", a, sep="= ")
+# a /= b
+print("a /= b es lo mismo que a = a / b", a, sep="= ")
+# a //= b
+print("a //= b es lo mismo que a = a // b", a, sep="= ")
+# a **= b
+
+
+print("""
+P -> Paréntesis
+
+E -> Exponenciación
+
+M -> Multiplicación
+
+D -> División
+
+A -> Adición
+
+S -> Sustracción
+""")
