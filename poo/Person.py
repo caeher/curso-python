aae848fba701901078649293cae6a4eb132825b9
Cristian Antonio Escalante Hernandez <eh18010@ues.edu.sv>
2026-03-22 14:55:24 -0600

    fix: update poo/Person.py

--- poo/Person.py
+++ poo/Person.py
@@ -11,3 +11,29 @@ person1 = Person("Cristian", 22)
 person2 = Person("Antonio", 25)
 
 person1.greet()
+
+
+class Person2:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+# Crear una instancia de Person
+person1 = Person2("Ana", 30)
+print(person1.name)
+print(person1.age) 
+
+
+class Student(Person):
+    def __init__(self, name, age, student_id):
+        super().__init__(name, age)
+        self.student_id = student_id
+
+    def greet(self):
+        super().greet()
+        print(f"Hi, I'm {self.name}, and I'm a student with ID: {self.student_id}")
+
+# Crear instancia de Student y llamar a greet
+student = Student("Ana", 20, "S12345")
+student.greet()  # Output: Hello! I am a person.
+                 #         Hi, I'm Ana, and I'm a student with ID: S12345
