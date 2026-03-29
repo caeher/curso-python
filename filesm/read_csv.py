201b83a85275245e9ea2cab55e0f06d4a2972525
Cristian Antonio Escalante Hernandez <eh18010@ues.edu.sv>
2026-03-29 17:19:58 -0600

    feat: archivos y libreria estandar de python

--- filesm/read_csv.py
+++ filesm/read_csv.py
@@ -24,3 +24,20 @@ new_product = {
 with open('products.csv', 'a') as file:
     csv_writer = csv.DictWriter(file, fieldnames=['name', 'price', 'quantity', 'category'])
     csv_writer.writerow(new_product)
+
+
+file_path = 'products.csv'
+updated_file_path = 'products_updated.csv'
+
+with open(file_path, mode='r') as file:
+    csv_reader = csv.DictReader(file)
+    #Obtener lo snombres de las columnas existentes
+    fieldnames = csv_reader.fieldnames + ['total_value']
+
+    with open(updated_file_path, mode='w') as file:
+        csv_writer = csv.DictWriter(file, fieldnames=fieldnames)
+        csv_writer.writeheader()
+
+        for row in csv_reader:
+            row['total_value'] = float(row['price']) * float(row['quantity'])
+            csv_writer.writerow(row)
