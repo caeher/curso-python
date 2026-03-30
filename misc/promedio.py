29183f67ab699a0aceb1dbc1612c6656d06ffee0
Cristian Antonio Escalante Hernandez <eh18010@ues.edu.sv>
2026-03-29 18:06:33 -0600

    feat: add misc/promedio.py file

---
+++ misc/promedio.py
@@ -0,0 +1,18 @@
+def calculate_average(numbers):
+    """
+    Calcula el promedio de una lista de números
+
+    Parámetros:
+    numbers (list): lista de números
+
+    Retorna:
+    float: promedio de los números
+    
+    """
+    return sum(numbers) / len(numbers)
+
+numbers = [1, 2, 3, 4, 5]
+print(calculate_average(numbers))
+
+numbers = [1, 2, 3, 4, 5]
+print(calculate_average(numbers))
